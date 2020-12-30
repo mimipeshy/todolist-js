38b145b5353cf54257c3433c07143058060b3c2e
mimipeshy <perisndanu@gmail.com>
2020-12-30 18:40:09 +0300

    Add delete project button

--- src/createProject.js
+++
@@ -1,3 +0,0 @@
-import landingPage from "./landing"
-import getLists from landingPage
-
--- src/landing.js
+++ src/landing.js
@@ -1,19 +1,18 @@
 const listContainer= document.querySelector('[data-lists]')
 const newProjectForm= document.querySelector('[data-new-list-form]')
 const newProjectInput= document.querySelector('[data-new-list-input]')
+const deleteListButon = document.querySelector('[data-delete-list-button]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
-const LOCAL_STORAGE_SELECTED_LIST_KEY ='task.selectedListIs'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
-let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_KEY)
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 
-listContainer.addEventListener('click', e=>{
-    e.preventDefault()
-    if(e.target.tagName.toLowerCase()==='li'){
-        selectedListId =e.target.dataset.listId
-        saveAndRender()
+listContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+      selectedListId = e.target.dataset.listId
+      saveAndRender()
     }
-
 })
 
 newProjectForm.addEventListener('submit',e =>{
@@ -26,11 +25,18 @@ newProjectForm.addEventListener('submit',e =>{
     saveAndRender()
 })
 
+deleteListButon.addEventListener('click', e=>{
+    lists= lists.filter(list => list.id !==selectedListId)
+    selectedListId= null
+    saveAndRender()
+})
+
 function createProject(name){
     return {id: Date.now().toString(), name: name, tasks:[]}
 }
 function saveItems(){
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
 }
 function saveAndRender(){
     saveItems()
@@ -39,7 +45,7 @@ function saveAndRender(){
 function getLists(){
    clearElement(listContainer)
     lists.forEach(list =>{
-      const newList= document.createElement('list-name')
+      const newList= document.createElement('li')
       newList.dataset.listId = list.id
       newList.classList.add('list-name')
       newList.innerText= list.name
