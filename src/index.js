17f9243975d8f883b623b799bbe5b613e8bb55ed
mimipeshy <perisndanu@gmail.com>
2021-01-13 18:47:13 +0300

    Initial dist subtree commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import './styles/style.css';
 import { renderAddTodo } from './display-add-todo';
 import projectDisplayListener from './project-functions';
 import showProjects from './show-projects';
 import { showTodos, showTodosInClickedProject } from './show-todos';
 import Storage from './storage';
-
 
 Storage.getProjects();
 renderAddTodo();
