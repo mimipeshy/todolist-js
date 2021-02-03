3568aa87a4325e9bc2d7a19bbf2edb8377c671dd
mimipeshy <perisndanu@gmail.com>
2021-02-03 13:45:51 +0300

    finished testing with jest|

--- tests/storage.test.js
+++
@@ -1,8 +0,0 @@
-import { getProjects } from '../src/storage';
-
-describe('Check local storage methods', () => {
-  const defaultProj = [{ title: 'Default Project', todos: [] }];
-  it('should return a default project Array', () => {
-    expect(getProjects()).toStrictEqual(defaultProj);
-  });
-});
